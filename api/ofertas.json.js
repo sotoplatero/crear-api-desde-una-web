fa77eddba6aede16803453e736017b12a6d6981c
Damian Soto <soto.platero@gmail.com>
2021-10-26 17:01:10 -0400

    first commit

---
+++ api/ofertas.json.js
@@ -0,0 +1,27 @@
+var got = require('got')
+var cheerio = require('cheerio')
+
+module.exports = async (req, res) => {
+
+  // tomamos el nombre desde  querystring del la url 
+  const urls = [
+    'http://ofertas.cu/c/390/pc/1.html',
+    'http://ofertas.cu/c/391/laptop/1.html',
+    'http://ofertas.cu/c/394/celulares-accesorios/1.html',
+  ]
+
+  let ads = []
+  for (var i = urls.length - 1; i >= 0; i--) {
+    const url = urls[i]
+    const body = await got(url).text()
+    // return 'url'
+    const $ = cheerio.load(body)
+    const ads = $('.listing.list-mode').map( node => ({
+        title: $(node).find('a').text(),
+        url: $(node).find('a').attr('href'),
+    }) ).toArray()
+  }
+
+  // retornamos un JSON con el saludo
+  res.json(ads);
+};
